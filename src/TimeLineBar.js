9c73491dcdc0b72ca47b4cd1a17b9ba4a9d76b83
Toolo <barick2@gmail.com>
2017-03-11 13:17:34 -0800

    rb-4: removed logo

--- src/TimeLineBar.js
+++ src/TimeLineBar.js
@@ -3,7 +3,6 @@ import './TimeLineBar.css';
 
 import {Range} from 'rc-slider';
 import 'rc-slider/assets/index.css';
-import logo from './logo.png';
 
 export default class TimeLineBar extends PureComponent {
     render() {
