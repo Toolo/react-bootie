912fe72b8893936d0580022b32b0a2d082a68605
Victor <victor@turo.com>
2017-03-11 12:25:33 -0800

    add leaflet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import logo from './logo.svg';
+import Map from './Map';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Dale
+      <div className="app">
+        <Map />
       </div>
     );
   }
---
+++ src/Map.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import './Map.css';
+
+export default class MapContainer extends Component {
+    render() {
+
+        const position = [37.7822, -122.3934];
+        return (
+            <Map center={position} zoom={15}>
+                <TileLayer
+                    url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                />
+                <Marker position={position}>
+                    <Popup>
+                        <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
+                    </Popup>
+                </Marker>
+            </Map>
+        );
+    }
+}
