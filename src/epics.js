ce96c6e870dceb5e1f59cfae6994e3a86dd8d295
Toolo <barick2@gmail.com>
2017-03-12 10:28:18 -0700

    rb-12
    - setCurrentEvent now centers the map on the selected event

--- src/epics.js
+++ src/epics.js
@@ -1,8 +1,9 @@
 import { Observable } from 'rxjs';
 import { combineEpics } from 'redux-observable';
-import { getEvents, getEventsFulfilled, updateFilterFulfilled, updateFilter, clearCurrentEvent} from './actions';
+import { getEvents, getEventsFulfilled, updateFilterFulfilled, updateFilter, clearCurrentEvent, updateMapPosition} from './actions';
 import constants from './constants';
 import EventsService from './EventsService';
+import { mapSelector } from './selectors';
 
 function getEventsEpic(action$) {
     return action$.ofType(constants.GET_EVENTS)
@@ -27,9 +28,19 @@ function updateTimeLineEpic(action$) {
         .map(action => getEvents(action.payload));
 }
 
-function setCurrentEventEpic(action$) {
-    return action$.ofType(constants.SET_CURRENT_EVENT)
+function setCurrentEventEpic(action$, store) {
+    const setCurrentEvent$ = action$.ofType(constants.SET_CURRENT_EVENT)
         .map(action => updateFilter({filter: action.payload.event.name}));
+    const updateMapPosition$ = action$.ofType(constants.SET_CURRENT_EVENT)
+        .map(action => {
+            const event = action.payload.event;
+            const map = mapSelector(store.getState());
+            return updateMapPosition({
+                center: [event.latitude, event.longitude],
+                zoom: map.zoom
+            });
+        });
+    return Observable.merge(setCurrentEvent$, updateMapPosition$);
 }
 
 export default combineEpics(getEventsEpic, updateFilterEpic, updateTimeLineEpic, setCurrentEventEpic);
